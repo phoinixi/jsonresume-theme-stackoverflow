4486d58968565cea8b03a5b81fc9ca4f79f35d35
Francesco Esposito <fenix@Francescos-MacBook-Pro-2.local>
2016-02-21 14:44:50 +0100

    updated depencies and bug fixing

--- index.js
+++ index.js
@@ -1,55 +1,57 @@
-var fs = require("fs");
-var Handlebars = require("handlebars");
-var moment = require("moment");
+(function() {
+    var fs = require("fs");
+    var Handlebars = require("handlebars");
+    var moment = require("moment");
 
-function render(resume) {
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
-	
-	return Handlebars.compile(tpl)({
-		css: css,
-		resume: resume
-	});
-}
-
-module.exports = {
-	render: render
-};
-
-
-/* HANDLEBARS HELPERS */
-Handlebars.registerHelper('paragraphSplit', function(plaintext) {
-    var lines = plaintext.split(/\r\n|\r|\n/g);
-    var output = '';
-    var i;
-
-    for (i = 0; i < lines.length; i+=1) {
-        if(lines[i]) {
-            output += '<p>' + lines[i] + '</p>';
-        }
+    function render(resume) {
+    	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+    	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+    	
+    	return Handlebars.compile(tpl)({
+    		css: css,
+    		resume: resume
+    	});
     }
-    return new Handlebars.SafeString(output);
-});
 
-Handlebars.registerHelper('toLowerCase', function(str) {
-  return str.toLowerCase();
-});
+    module.exports = {
+    	render: render
+    };
 
-Handlebars.registerHelper('spaceToDash', function(str) {
-  return str.replace(/\s/g, '-').toLowerCase();
-});
 
-Handlebars.registerHelper('MY', function(date) {
-	var d = date.toString();
-	return moment(d).format('MMMM YYYY');
-});
+    /* HANDLEBARS HELPERS */
+    Handlebars.registerHelper('paragraphSplit', function(plaintext) {
+        var lines = plaintext.split(/\r\n|\r|\n/g);
+        var output = '';
+        var i;
 
-Handlebars.registerHelper('Y', function(date) {
-	var d = date.toString();
-  return moment(d).format('YYYY');
-});
+        for (i = 0; i < lines.length; i+=1) {
+            if(lines[i]) {
+                output += '<p>' + lines[i] + '</p>';
+            }
+        }
+        return new Handlebars.SafeString(output);
+    });
 
-Handlebars.registerHelper('DMY', function(date) {
-	var d = date.toString();
-  return moment(d).format('D MMMM YYYY');
-});
+    Handlebars.registerHelper('toLowerCase', function(str) {
+      return str.toLowerCase();
+    });
+
+    Handlebars.registerHelper('spaceToDash', function(str) {
+      return str.replace(/\s/g, '-').toLowerCase();
+    });
+
+    Handlebars.registerHelper('MY', function(date) {
+    	var d = date.toString();
+    	return moment(d, 'YYYY-mm-dd').format('MMMM YYYY');
+    });
+
+    Handlebars.registerHelper('Y', function(date) {
+    	var d = date.toString();
+        return moment(d, 'YYYY-mm-dd').format('YYYY');
+    });
+
+    Handlebars.registerHelper('DMY', function(date) {
+    	var d = date.toString();
+        return moment(d, 'YYYY-mm-dd').format('D MMMM YYYY');
+    });
+}());
