17990be1f37008cf0a9df64b1d7fd820597fccdd
francescoes <fraesps@gmail.com>
2017-06-29 12:43:19 +0200

    refactoring of handlebars according to the new json-resume-template (html splitted in partials)

--- index.js
+++ index.js
@@ -1,81 +1,92 @@
-(function() {
-  var fs = require("fs");
-  var Handlebars = require("handlebars");
-  var moment = require("moment");
+var fs = require('fs');
+var path = require('path');
+var Handlebars = require('handlebars');
+var moment = require('moment');
 
-  function render(resume) {
-    var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-    var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+function render(resume) {
+	var css = fs.readFileSync(__dirname + '/style.css', 'utf-8');
+	var tpl = fs.readFileSync(__dirname + '/resume.hbs', 'utf-8');
+	var partialsDir = path.join(__dirname, 'partials');
+	var filenames = fs.readdirSync(partialsDir);
 
-    return Handlebars.compile(tpl)({
-      css: css,
-      resume: resume
-    });
-  }
+	filenames.forEach(function (filename) {
+		var matches = /^([^.]+).hbs$/.exec(filename);
+		if (!matches) {
+			return;
+		}
+		var name = matches[1];
+		var filepath = path.join(partialsDir, filename)
+		var template = fs.readFileSync(filepath, 'utf8');
 
-  module.exports = {
-    render: render
-  };
+		Handlebars.registerPartial(name, template);
+	});
+	return Handlebars.compile(tpl)({
+		css: css,
+		resume: resume
+	});
+}
 
+/* HANDLEBARS HELPERS */
+Handlebars.registerHelper('paragraphSplit', function (plaintext) {
+	var output = '';
+	var lines = plaintext instanceof Array ? plaintext.join('').split(/\r\n|\r|\n/g) : plaintext.split(/\r\n|\r|\n/g);
+	var i = 0;
 
-  /* HANDLEBARS HELPERS */
-  Handlebars.registerHelper('paragraphSplit', function(plaintext) {
-    var output = '';
-    var lines = plaintext instanceof Array ? plaintext.join('').split(/\r\n|\r|\n/g) : plaintext.split(/\r\n|\r|\n/g);
-    var i = 0;
+	while (i < lines.length) {
+		if (lines[i]) {
+			output += '<p>' + lines[i] + '</p>';
+		}
+		i += 1;
+	}
 
-    while(i < lines.length) {
-      if(lines[i]) {
-        output += '<p>' + lines[i] + '</p>';
-      }
-      i += 1;
-    }
+	return new Handlebars.SafeString(output);
+});
 
-    return new Handlebars.SafeString(output);
-  });
+Handlebars.registerHelper('toLowerCase', function (str) {
+	return str.toLowerCase();
+});
 
-  Handlebars.registerHelper('toLowerCase', function(str) {
-    return str.toLowerCase();
-  });
+Handlebars.registerHelper('spaceToDash', function (str) {
+	return str.replace(/\s/g, '-').toLowerCase();
+});
 
-  Handlebars.registerHelper('spaceToDash', function(str) {
-    return str.replace(/\s/g, '-').toLowerCase();
-  });
+Handlebars.registerHelper('MY', function (date) {
+	return moment(date.toString(), ['YYYY-MM-DD']).format('MMMM YYYY');
+});
 
-  Handlebars.registerHelper('MY', function(date) {
-    return moment(date.toString(), ['YYYY-MM-DD']).format('MMMM YYYY');
-  });
+Handlebars.registerHelper('Y', function (date) {
+	return moment(date.toString(), ['YYYY-MM-DD']).format('YYYY');
+});
 
-  Handlebars.registerHelper('Y', function(date) {
-    return moment(date.toString(), ['YYYY-MM-DD']).format('YYYY');
-  });
+Handlebars.registerHelper('DMY', function (date) {
+	return moment(date.toString(), ['YYYY-MM-DD']).format('D MMMM YYYY');
+});
 
-  Handlebars.registerHelper('DMY', function(date) {
-    return moment(date.toString(), ['YYYY-MM-DD']).format('D MMMM YYYY');
-  });
+Handlebars.registerHelper('birthData', function (birth) {
+	var out = [];
+	if (birth && Object.keys(birth).length) {
+		out.push('<div> Born in ');
+		out.push(birth.place);
 
-  Handlebars.registerHelper('birthData', function(birth) {
-    var out = [];
-    if (birth && Object.keys(birth).length) {
-      out.push('<div> Born in ');
-      out.push(birth.place);
+		if (birth.place && birth.state) {
+			out.push(', ');
+		}
 
-      if (birth.place && birth.state) {
-        out.push(', ');
-      }
+		out.push(birth.state);
 
-      out.push(birth.state);
+		var year = birth.date &&
+			moment(birth.date.toString(), ['YYYY-MM-DD']).format('YYYY');
 
-      var year = birth.date &&
-        moment(birth.date.toString(), ['YYYY-MM-DD']).format('YYYY');
+		if (year && (birth.place || birth.state)) {
+			out.push(' in ');
+		}
+		out.push(year);
+		out.push('</div>');
+	}
 
-      if (year && (birth.place || birth.state)) {
-        out.push(' in ');
-      }
-      out.push(year);
-      out.push('</div>');
-    }
+	return new Handlebars.SafeString(out.join(''));
+});
 
-    return new Handlebars.SafeString(out.join(''));
-  });
-}());
+module.exports = {
+	render: render
+};
