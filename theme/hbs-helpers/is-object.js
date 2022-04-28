f39d65de04aad1b3da776994efe2f6874a9af021
Francesco Esposito <fresposi@microsoft.com>
2022-04-28 18:36:57 +0200

    bug fixes and update to json-schema v1

--- theme/hbs-helpers/is-object.js
+++
@@ -1,9 +0,0 @@
-const isObject = function (item, options) {
-  if (typeof item === 'object' && item !== null) {
-    return options.fn(this);
-  } else {
-    return options.inverse(this);
-  }
-};
-
-module.exports = { isObject };
