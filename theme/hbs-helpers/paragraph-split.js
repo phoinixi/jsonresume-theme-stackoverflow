bcf26c85ec38ffab55268ae92cc6c8abc8166df3
Arnaud <arnaud.bruyer@gmail.com>
2022-11-09 13:46:43 +0900

    Added markdown capability

--- theme/hbs-helpers/paragraph-split.js
+++ theme/hbs-helpers/paragraph-split.js
@@ -1,7 +1,15 @@
 const { SafeString } = require('handlebars');
+const markdown = require('markdown-it')({
+  breaks: true
+});
 
 const paragraphSplit = (text) => {
   const expr = /\r\n|\r|\n/g;
+
+  if (text != null) {
+    text = markdown.render(text);
+  }
+
   const lines = Array.isArray(text) ? text.join('').split(expr) : text.split(expr);
   const output = lines.filter(line => line).reduce((a, b) => `${a}<p>${b}</p>`, '');
   return new SafeString(output);
