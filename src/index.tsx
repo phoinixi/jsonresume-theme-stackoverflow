b7efae959e5e330155878e675b8c5ce44779830f
Levin Keller <post@levinkeller.de>
2023-04-28 13:40:48 +0200

    fix: fix some regressions from migration to react

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,8 @@ const createRender = (language: Language) => (resume: ResumeType) =>
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My App</title>
+    <title>${resume.basics?.name || "Resume"}}</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <style>${css}</style>
   </head>
   <body>
